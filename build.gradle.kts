4355818a2725701a689f680b0874334b4f2820cc
Tobias Gesellchen <tobias@gesellix.de>
2018-12-29 17:08:04 +0100

    Convert to Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,127 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.text.SimpleDateFormat
+import java.util.*
+
+val kotlinVersion = plugins.getPlugin(KotlinPluginWrapper::class.java).kotlinPluginVersion
+rootProject.extra.set("artifactVersion", SimpleDateFormat("yyyy-MM-dd\'T\'HH-mm-ss").format(Date()))
+rootProject.extra.set("bintrayDryRun", false)
+
+buildscript {
+    repositories {
+        mavenLocal()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.3.11"
+    `maven-publish`
+    id("com.github.ben-manes.versions") version "0.20.0"
+    id("com.jfrog.bintray") version "1.8.4"
+    id("net.ossindex.audit") version "0.3.21"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType(Test::class.java) {
+        useJUnitPlatform {
+            includeEngines("spek")
+        }
+    }
+
+    bintrayUpload {
+        dependsOn("build")
+    }
+
+    register<Wrapper>("updateWrapper") {
+        gradleVersion = "5.0"
+        distributionType = Wrapper.DistributionType.ALL
+    }
+}
+
+val dependencyVersions = listOf(
+)
+
+configurations.all {
+    resolutionStrategy {
+        failOnVersionConflict()
+        force(dependencyVersions)
+    }
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven { setUrl("http://dl.bintray.com/jetbrains/spek") }
+    mavenCentral()
+}
+
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+
+    compile("org.slf4j:slf4j-api:1.7.25")
+    compile("ch.qos.logback:logback-classic:1.2.3")
+
+    testCompile("org.jetbrains.kotlin:kotlin-test")
+    testCompile("org.jetbrains.spek:spek-api:1.2.1") {
+        exclude("org.jetbrains.kotlin")
+    }
+    testRuntime("org.jetbrains.spek:spek-junit-platform-engine:1.2.1") {
+        exclude("org.junit.platform")
+        exclude("org.jetbrains.kotlin")
+    }
+    testRuntime("org.junit.platform:junit-platform-launcher:1.3.2")
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    dependsOn("classes")
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+artifacts {
+    add("archives", sourcesJar.get())
+}
+
+val publicationName = "testutil"
+publishing {
+    publications {
+        register(publicationName, MavenPublication::class) {
+            groupId = "de.gesellix"
+            artifactId = "testutil"
+            version = rootProject.extra["artifactVersion"] as String
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
+
+fun findProperty(s: String) = project.findProperty(s) as String?
+
+bintray {
+    user = System.getenv()["BINTRAY_USER"] ?: findProperty("bintray.user")
+    key = System.getenv()["BINTRAY_API_KEY"] ?: findProperty("bintray.key")
+    setPublications(publicationName)
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "docker-utils"
+        name = "testutil"
+        desc = "Test utilities for the docker-utils libraries"
+        setLicenses("MIT")
+        setLabels("docker", "util", "test", "java")
+        version.name = rootProject.extra["artifactVersion"] as String
+        vcsUrl = "https://github.com/docker-client/testutil.git"
+    })
+    dryRun = rootProject.extra["bintrayDryRun"] as Boolean
+}
