49f43b6319cce76e08e9795958eeff72ba6f45fc
Tobias Gesellchen <tobias@gesellix.de>
2021-06-06 18:17:17 +0200

    Update to Groovy 3.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ dependencies {
   }
   api("ch.qos.logback:logback-classic")
   implementation("org.slf4j:slf4j-api")
-  testImplementation("org.codehaus.groovy:groovy:(2.5,3]!!2.5.14")
-  testImplementation("org.spockframework:spock-core:2.0-groovy-2.5")
+  testImplementation("org.codehaus.groovy:groovy:(3,4]!!3.0.8")
+  testImplementation("org.spockframework:spock-core:2.0-groovy-3.0")
 }
 
 val dependencyVersions = listOf<String>(
