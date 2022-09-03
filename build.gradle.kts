d0584b3ac9800966d8a1620a29e307e8731fa8fd
Tobias Gesellchen <tobias@gesellix.de>
2022-09-03 14:10:51 +0200

    cleanup dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,8 @@ repositories {
 }
 
 dependencies {
-  api("ch.qos.logback:logback-classic:[1.2,1.3)!!1.2.11")
-  implementation("org.slf4j:slf4j-api:[1.7,1.8)!!1.7.36")
-  testImplementation("org.codehaus.groovy:groovy:[3,4)!!3.0.11")
+  api("ch.qos.logback:logback-classic:[1.2,2)!!1.2.11")
+  implementation("org.slf4j:slf4j-api:[1.7,2)!!1.7.36")
   testImplementation("org.spockframework:spock-core:2.2-groovy-4.0")
 }
 
