3bf4172f37f8b2b8df39a11110af018c2c1d6376
Tobias Gesellchen <tobias@gesellix.de>
2022-10-03 21:02:06 +0200

    Bump to logback-classic 1.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-  api("ch.qos.logback:logback-classic:[1.2,2)!!1.3.1")
+  api("ch.qos.logback:logback-classic:[1.2,2)!!1.3.3")
   implementation("org.slf4j:slf4j-api:[1.7,3)!!2.0.3")
   testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
 }
