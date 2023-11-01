2de046270e720f134790b9199970a46cf6f16167
Tobias Gesellchen <tobias@gesellix.de>
2023-11-01 10:44:03 +0100

    Bump preferred dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ repositories {
 }
 
 dependencies {
-  api("ch.qos.logback:logback-classic:[1.2,2)!!1.3.8")
-  implementation("org.slf4j:slf4j-api:[1.7,3)!!2.0.7")
+  api("ch.qos.logback:logback-classic:[1.2,2)!!1.3.11")
+  implementation("org.slf4j:slf4j-api:[1.7,3)!!2.0.9")
   testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
 }
 
